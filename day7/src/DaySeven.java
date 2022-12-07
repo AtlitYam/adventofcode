acfc06bb4b7cce0b909721e8fb2ea17fd029cb7e
Atlit <atlit@outlook.com>
2022-12-08 00:38:23 +0100

    Changed to the correct int in challenge 2

--- day7/src/DaySeven.java
+++ day7/src/DaySeven.java
@@ -20,10 +20,10 @@ public class DaySeven {
         Integer requiredSpaceForUpdate = (int) (3e7 - freeSystemSpace);
 
         // For the first challenge we need to find the folders larger than 100.000 and add those up
-        System.out.printf("The total size of all folders smaller than 100.000 is %s%n", DaySevenImpl.getTotalSizeOfFoldersSmallerThan(folders, files, (int) 1e5));
+        System.out.printf("The total size of all folders smaller than 100000 is %s%n", DaySevenImpl.getTotalSizeOfFoldersSmallerThan(folders, files, (int) 1e5));
 
         // For the second challenge we need to find the smallest folder of at least 8.381.165
-        System.out.printf("The smallest folder of at least 8.381.165 has a size of %s%n", DaySevenImpl.getFirstFolderOfAtleastSize(folders, files, requiredSpaceForUpdate));
+        System.out.printf("The smallest folder of at least %s has a size of %s%n", requiredSpaceForUpdate, DaySevenImpl.getFirstFolderOfAtleastSize(folders, files, requiredSpaceForUpdate));
 
         System.out.printf("(Completed in %sms)%n", Duration.between(startTime, Instant.now()).toMillis());
     }
