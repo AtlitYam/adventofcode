79bbe0326f6a8cc51c0d62a8c5ae91fa22cad733
Atlit <atlit@outlook.com>
2022-12-10 22:44:12 +0100

    Completed Day Ten with known bug

--- day10/src/DayTen.java
+++ day10/src/DayTen.java
@@ -1,11 +1,26 @@
+import fileutils.FileLoader;
+
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 
 public class DayTen {
     public static void main(String[] args) {
         Instant startTime = Instant.now();
-        String filePath = "resources/DayTenExample.txt";
+        String filePath = "resources/DayTenChallenge.txt";
+        List<String> file = FileLoader.loadFileAsList(filePath);
 
+        // For the first challenge we need to get the sum of the 20th, 60th, 100th, 140th, 180th and 220th cycle multiplied by the X at the time
+        System.out.printf("The total signal strength during the check cycles is %s%n", DayTenImpl.getTotalSignalStrength(file));
+
+        // For the second challenge we need to draw out the CRT by measuring the sprite position and where the CRT is drawing
+        String crtOutput = DayTenImpl.getCRTOutput(file);
+        System.out.printf("%s%n", crtOutput.substring(0, 40));
+        System.out.printf("%s%n", crtOutput.substring(40, 80));
+        System.out.printf("%s%n", crtOutput.substring(80, 120));
+        System.out.printf("%s%n", crtOutput.substring(120, 160));
+        System.out.printf("%s%n", crtOutput.substring(160, 200));
+        System.out.printf("%s%n", crtOutput.substring(200, 240));
 
         System.out.printf("(Completed in %sms)%n", Duration.between(startTime, Instant.now()).toMillis());
     }
--- day10/src/DayTenImpl.java
+++ day10/src/DayTenImpl.java
@@ -1,2 +1,203 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class DayTenImpl {
+    public static Integer getTotalSignalStrength(List<String> commands) {
+        int X = 1;
+        int cycles = 0;
+        HashMap<Integer, Integer> signalStrengths = new HashMap<>();
+        List<Integer> checkingCycles = List.of(20, 60, 100, 140, 180, 220);
+        for (String command : commands) {
+            String[] splitCommand = command.split(" ");
+            switch (splitCommand[0]) {
+                case "noop" -> {
+                    if (!signalStrengths.containsKey(cycles + 1)) {
+                        if (checkingCycles.contains(cycles + 1)) {
+                            signalStrengths.put(cycles + 1, X * (cycles + 1));
+                        }
+                    }
+                    cycles++;
+                    continue;
+                }
+                case "addx" -> {
+                    if (!signalStrengths.containsKey(cycles + 1)) {
+                        if (checkingCycles.contains(cycles + 1)) {
+                            signalStrengths.put(cycles + 1, X * (cycles + 1));
+                        }
+                    }
+
+                    if (!signalStrengths.containsKey(cycles + 2)) {
+                        if (checkingCycles.contains(cycles + 2)) {
+                            signalStrengths.put(cycles + 2, X * (cycles + 2));
+                        }
+                    }
+
+                    X += Integer.parseInt(splitCommand[1]);
+                    cycles += 2;
+                }
+            }
+
+            if (!signalStrengths.containsKey(cycles)) {
+                if (checkingCycles.contains(cycles)) {
+                    signalStrengths.put(cycles, X * cycles);
+                }
+            }
+        }
+        return signalStrengths.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public static String getCRTOutput(List<String> commands) {
+        int X = 1;
+        int cycles = 0;
+        HashMap<Integer, String> crtOutput = new HashMap<>();
+        for (String command : commands) {
+            String[] splitCommand = command.split(" ");
+            switch (splitCommand[0]) {
+                case "noop":
+//                    if (X - 1 == cycles || X == cycles || X + 1 == cycles) {
+//                        crtOutput.put(cycles, "#");
+//                    } else {
+//                        crtOutput.put(cycles, ".");
+//                    }
+//                    if (X - 1 == cycles + 1 || X == cycles + 1 || X + 1 == cycles + 1) {
+//                        crtOutput.put(cycles + 1, "#");
+//                    } else {
+//                        crtOutput.put(cycles + 1, ".");
+//                    }
+                    updateCRT(cycles, X, crtOutput);
+                    cycles++;
+                    continue;
+                case "addx":
+//                    if (X - 1 == cycles || X == cycles || X + 1 == cycles) {
+//                        crtOutput.put(cycles, "#");
+//                    } else {
+//                        crtOutput.put(cycles, ".");
+//                    }
+//                    if (X - 1 == cycles + 1 || X == cycles + 1 || X + 1 == cycles + 1) {
+//                        crtOutput.put(cycles + 1, "#");
+//                    } else {
+//                        crtOutput.put(cycles + 1, ".");
+//                    }
+//                    if (X - 1 == cycles + 2 || X == cycles + 2 || X + 1 == cycles + 2) {
+//                        crtOutput.put(cycles + 2, "#");
+//                    } else {
+//                        crtOutput.put(cycles + 2, ".");
+//                    }
+                    updateCRT(cycles, X, crtOutput);
+                    X += Integer.parseInt(splitCommand[1]);
+                    cycles += 2;
+            }
+            updateCRT(cycles, X, crtOutput);
+        }
+        return String.join("", crtOutput.values());
+    }
+
+    public static void updateCRT(Integer cycles, Integer X, HashMap<Integer, String> crtOutput) {
+        if (cycles < 40) {
+            if (X - 1 == cycles || X == cycles || X + 1 == cycles) {
+                crtOutput.put(cycles, "#");
+            } else {
+                crtOutput.put(cycles, ".");
+            }
+            if (X - 1 == cycles + 1 || X == cycles + 1 || X + 1 == cycles + 1) {
+                crtOutput.put(cycles + 1, "#");
+            } else {
+                crtOutput.put(cycles + 1, ".");
+            }
+            if (X - 1 == cycles + 2 || X == cycles + 2 || X + 1 == cycles + 2) {
+                crtOutput.put(cycles + 2, "#");
+            } else {
+                crtOutput.put(cycles + 2, ".");
+            }
+        }
+        if (cycles >= 39 & cycles < 80) {
+            if (X - 1 == cycles - 40 || X == cycles - 40 || X + 1 == cycles - 40) {
+                crtOutput.put(cycles, "#");
+            } else {
+                crtOutput.put(cycles, ".");
+            }
+            if (X - 1 == cycles + 1 - 40 || X == cycles + 1 - 40 || X + 1 == cycles + 1 - 40) {
+                crtOutput.put(cycles + 1, "#");
+            } else {
+                crtOutput.put(cycles + 1, ".");
+            }
+            if (X - 1 == cycles + 2 - 40 || X == cycles + 2 - 40 || X + 1 == cycles + 2 - 40) {
+                crtOutput.put(cycles + 2, "#");
+            } else {
+                crtOutput.put(cycles + 2, ".");
+            }
+        }
+
+        if (cycles >= 79 & cycles < 120) {
+            if (X - 1 == cycles - 80 || X == cycles - 80 || X + 1 == cycles - 80) {
+                crtOutput.put(cycles, "#");
+            } else {
+                crtOutput.put(cycles, ".");
+            }
+            if (X - 1 == cycles + 1 - 80 || X == cycles + 1 - 80 || X + 1 == cycles + 1 - 80) {
+                crtOutput.put(cycles + 1, "#");
+            } else {
+                crtOutput.put(cycles + 1, ".");
+            }
+            if (X - 1 == cycles + 2 - 80 || X == cycles + 2 - 80 || X + 1 == cycles + 2 - 80) {
+                crtOutput.put(cycles + 2, "#");
+            } else {
+                crtOutput.put(cycles + 2, ".");
+            }
+        }
+
+        if (cycles >= 119 & cycles < 160) {
+            if (X - 1 == cycles - 120 || X == cycles - 120 || X + 1 == cycles - 120) {
+                crtOutput.put(cycles, "#");
+            } else {
+                crtOutput.put(cycles, ".");
+            }
+            if (X - 1 == cycles + 1 - 120 || X == cycles + 1 - 120 || X + 1 == cycles + 1 - 120) {
+                crtOutput.put(cycles + 1, "#");
+            } else {
+                crtOutput.put(cycles + 1, ".");
+            }
+            if (X - 1 == cycles + 2 - 120 || X == cycles + 2 - 120 || X + 1 == cycles + 2 - 120) {
+                crtOutput.put(cycles + 2, "#");
+            } else {
+                crtOutput.put(cycles + 2, ".");
+            }
+        }
+
+        if (cycles >= 159 & cycles < 200) {
+            if (X - 1 == cycles - 160 || X == cycles - 160 || X + 1 == cycles - 160) {
+                crtOutput.put(cycles, "#");
+            } else {
+                crtOutput.put(cycles, ".");
+            }
+            if (X - 1 == cycles + 1 - 160 || X == cycles + 1 - 160 || X + 1 == cycles + 1 - 160) {
+                crtOutput.put(cycles + 1, "#");
+            } else {
+                crtOutput.put(cycles + 1, ".");
+            }
+            if (X - 1 == cycles + 2 - 160 || X == cycles + 2 - 160 || X + 1 == cycles + 2 - 160) {
+                crtOutput.put(cycles + 2, "#");
+            } else {
+                crtOutput.put(cycles + 2, ".");
+            }
+        }
+
+        if (cycles >= 199 & cycles < 241) {
+            if (X - 1 == cycles - 200 || X == cycles - 200 || X + 1 == cycles - 200) {
+                crtOutput.put(cycles, "#");
+            } else {
+                crtOutput.put(cycles, ".");
+            }
+            if (X - 1 == cycles + (1 - 200) || X == cycles + (1 - 200) || X + 1 == cycles + (1 - 200)) {
+                crtOutput.put(cycles + 1, "#");
+            } else {
+                crtOutput.put(cycles + 1, ".");
+            }
+            if (X - 1 == cycles + (2 - 200) || X == cycles + (2 - 200) || X + 1 == cycles + (2 - 200)) {
+                crtOutput.put(cycles + 2, "#");
+            } else {
+                crtOutput.put(cycles + 2, ".");
+            }
+        }
+    }
 }
