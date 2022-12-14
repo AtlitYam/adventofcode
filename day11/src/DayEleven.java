001cf2d9be5cb1e6f73ac45573aaca65dffe9fa6
Atlit <atlit@outlook.com>
2022-12-14 01:09:55 +0100

    Added expected results for example

--- day11/src/DayEleven.java
+++ day11/src/DayEleven.java
@@ -17,7 +17,7 @@ public class DayEleven {
         // For challenge two we have to find the activity of the two most active monkeys after 10.000 rounds, while no lowering our worry levels
         // This code fails because it cannot handle the numbers as they become too large to be handled normally by java variables and require higher math to track
         System.out.printf("The result of the activity of the most active two monkeys is %s%n", DayElevenImpl.getActivityForTwoMostActive(file, 10_000, 1L));
-        System.out.printf("The expected result is %s%n", 14314925001L);
+        System.out.printf("The expected challenge result is %s. The expected example result is %s.%n", 14314925001L, 2713310158L);
 
         System.out.printf("(Completed in %sms)%n", Duration.between(startTime, Instant.now()).toMillis());
     }
